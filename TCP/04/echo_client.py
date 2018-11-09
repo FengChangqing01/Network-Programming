867f53cbc6120192defa03c24d2be0186ff8bbde
fwx589413 <fengchangqing1@huawei.com>
2018-11-09 18:02:31 +0800

    update

--- TCP/04/echo_client.py
+++ TCP/04/echo_client.py
@@ -1,15 +1,16 @@
 import socket
 
 clnt_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+clnt_sock.connect(('localhost', 9190))
+print("Connecting....")
 
 while True:
-    clnt_sock.connect(('localhost', 9190))
+    message = input('Input message(Q to quite): ')
+    if message == 'Q':
+        break
 
-    clnt_sock.send("Hello")
-
+    clnt_sock.send(message)
     data = clnt_sock.recv(1024)
-
     print('Message from server : ', data)
-
 
 clnt_sock.close()
