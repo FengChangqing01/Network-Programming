a02083f346d0a17a5d7136208f5cf13a565d0c75
fwx589413 <fengchangqing1@huawei.com>
2018-11-12 20:32:09 +0800

    upsdtae

--- TCP/06/uecho_client.py
+++ TCP/06/uecho_client.py
@@ -1,11 +1,13 @@
 import socket
 
 clent_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clent_sock.connect(('localhost', 9190))
 
 while True:
     message = input('Input meaasge: ')
     if message == 'Q':
         break
-    clent_sock.sendto(message.encode('ascii'), ('localhost', 9190))
+    # clent_sock.sendto(message.encode('ascii'), ('localhost', 9190))
+    clent_sock.sendall(message.encode('ascii'))
     data = clent_sock.recv(1024)
     print('Message form server : ', data)
