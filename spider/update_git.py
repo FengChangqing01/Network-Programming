52bbfad89a2ae5c990687637c3660dd41480db58
fwx589413 <fengchangqing1@huawei.com>
2018-12-06 12:35:10 +0800

    fix bug of cannot update cur_update

--- spider/update_git.py
+++ spider/update_git.py
@@ -3,12 +3,14 @@ import webbrowser, time
 web_page="https://github.com/FengChangqing01/Network-Programming"
 api = "https://api.github.com/repos/FengChangqing01/Network-Programming"
 
-"""get info"""
-all_info = requests.get(api).json()
-last_update = "2018-12-03T11:38:06Z"
-cur_update = all_info['updated_at']
+
 
 while True:
+    """get info"""
+    all_info = requests.get(api).json()
+    last_update = None
+    cur_update = all_info['updated_at']
+
     if not last_update:
         last_update = cur_update
 
