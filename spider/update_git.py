cfba16931b191b2640dccab188ad871156773606
fwx589413 <fengchangqing1@huawei.com>
2018-12-06 17:08:44 +0800

    add exception

--- spider/update_git.py
+++ spider/update_git.py
@@ -7,8 +7,13 @@ api = "https://api.github.com/repos/FengChangqing01/Network-Programming"
 last_update = None
 while True:
     """get info"""
-    all_info = requests.get(api).json()
-    cur_update = all_info['updated_at']
+    all_info = requests.get(api)
+    try:
+        all_info.raise_for_status()
+    except Exception as exc:
+        print("There was a problom: %s"%(exc))
+        continue
+    cur_update = all_info.json()['updated_at']
 
     if not last_update:
         last_update = cur_update
