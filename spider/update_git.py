5e7959c36f41129575d2b2049b257af5b344d994
fwx589413 <fengchangqing1@huawei.com>
2018-12-06 12:42:26 +0800

    ssss

--- spider/update_git.py
+++ spider/update_git.py
@@ -4,11 +4,10 @@ web_page="https://github.com/FengChangqing01/Network-Programming"
 api = "https://api.github.com/repos/FengChangqing01/Network-Programming"
 
 
-
+last_update = None
 while True:
     """get info"""
     all_info = requests.get(api).json()
-    last_update = None
     cur_update = all_info['updated_at']
 
     if not last_update:
