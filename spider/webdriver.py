1ece2ad7dae42308599c9f8c2685c3bacc71d993
fwx589413 <fengchangqing1@huawei.com>
2018-12-06 19:02:59 +0800

    Update

---
+++ spider/webdriver.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+def start_chorme():
+    driver = webdriver.Chrome(executable_path="spider\chromedriver.exe")
+    driver.start_client()
+    return driver
+
+start_chorme()
